d38bc1bd98e41c67135e03ef3e23c397aa687fdd
Angelo Lima Dias <angelolimadias@gmail.com>
2018-12-05 18:51:49 -0200

    fix bug

--- src/app/container/who-we-are/who-we-are.component.ts
+++ src/app/container/who-we-are/who-we-are.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { LANG } from '../../theme/pt';
-=======
 import { LANG } from '../../theme/pt'
 
->>>>>>> master
 @Component({
   selector: 'app-who-we-are',
   templateUrl: './who-we-are.component.html',
@@ -12,8 +8,6 @@ import { LANG } from '../../theme/pt'
 })
 export class WhoWeAreComponent implements OnInit {
   public lang = LANG
-
-  public lang = LANG;
 
   constructor() { }
 
