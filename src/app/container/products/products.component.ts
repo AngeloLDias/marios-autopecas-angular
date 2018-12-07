76e26eb79cd1ddf3cd36c1b5c022bf5d7ebf2053
Angelo Lima Dias <angelolimadias@gmail.com>
2018-12-07 20:06:42 -0200

    screen produtos -WIP

--- src/app/container/products/products.component.ts
+++ src/app/container/products/products.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LANG } from '../../theme/pt'
+import { Produtos } from '../../model/pruducts';
+
 
 @Component({
   selector: 'app-products',
@@ -8,8 +10,22 @@ import { LANG } from '../../theme/pt'
 })
 export class ProductsComponent implements OnInit {
   public lang = LANG
+  public produtos: Produtos[];
 
-  constructor() { }
+  constructor() {
+    
+    this.produtos = [
+      { 'nome': 'VOLANTE DE DIREÇÃO ORIGINAL VOLKSWAGEN SEM TECLA DE BUZINA GOL VOYAGE SAVEIRO G6 2013 EM DIANTE', 'tipo': 'Direção', 'preco': 180, 'quantidade': 19, 'marca': 'VOLKSWAGEN', 'imagem': ['../../../assets/media/volante.jpg'] },
+
+      { 'nome': 'BARRA DE DIREÇÃO AXIAL COM ROSCA FINA FIAT MOBI NOVO UNO NOVO PALIO NOVO FIORINO E GRAND SIENA TODAS AS VERSÕES MECÂNICO E HIDRÁULICO', 'tipo': 'Direção', 'preco': 188.50, 'quantidade': 9, 'marca': 'AXIAL', 'imagem': ['../../../assets/media/barra.jpg'] },
+
+      {
+        'nome': `MACACO JACARÉ PORTÁTIL 2 TONELADAS COM MALETA`, 'tipo': 'Ferramentas', 'preco': 137.00, 'quantidade': 10, 'marca': 'JACARÉ', 'imagem': ['../../../assets/media/Macaco.jpg']
+      },
+
+
+    ];
+  }
 
   ngOnInit() {
   }
