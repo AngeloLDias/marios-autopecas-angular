12c2bb5b61ef2c2cf4021b595709f11c604738d3
Angelo Lima Dias <angelolimadias@gmail.com>
2018-12-08 05:56:53 -0200

    error httpclient

--- src/app/container/list-products/list-products.component.ts
+++ src/app/container/list-products/list-products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Produtos } from '../../model/pruducts';
+import { ProductsService } from '../../services/products.service' 
 @Component({
   selector: 'app-list-products',
   templateUrl: './list-products.component.html',
@@ -20,24 +21,30 @@ export class ListProductsComponent implements OnInit {
   public showModalDontFound: boolean = false;
 
 
-  constructor() {
-
-    this.produtos = [
-      { 'nome': 'VOLANTE DE DIREÇÃO ORIGINAL VOLKSWAGEN SEM TECLA DE BUZINA GOL VOYAGE SAVEIRO G6 2013 EM DIANTE', 'tipo': 'Direção', 'preco': 180, 'quantidade': 19, 'marca': 'VOLKSWAGEN', 'imagem': ['../../../assets/media/volante.jpg'] },
-
-      { 'nome': 'BARRA DE DIREÇÃO AXIAL COM ROSCA FINA FIAT MOBI NOVO UNO NOVO PALIO NOVO FIORINO E GRAND SIENA TODAS AS VERSÕES MECÂNICO E HIDRÁULICO', 'tipo': 'Direção', 'preco': 188.50, 'quantidade': 9, 'marca': 'AXIAL', 'imagem': ['../../../assets/media/barra.jpg'] },
-
-      {
-        'nome': `MACACO JACARÉ PORTÁTIL 2 TONELADAS COM MALETA`, 'tipo': 'Ferramentas', 'preco': 137.00, 'quantidade': 10, 'marca': 'JACARÉ', 'imagem': ['../../../assets/media/Macaco.jpg']
-      },
+  constructor(private productsService : ProductsService) {
 
 
-    ];
+
+    // this.produtos = [
+    //   { 'nome': 'VOLANTE DE DIREÇÃO ORIGINAL VOLKSWAGEN SEM TECLA DE BUZINA GOL VOYAGE SAVEIRO G6 2013 EM DIANTE', 'tipo': 'Direção', 'preco': 180, 'quantidade': 19, 'marca': 'VOLKSWAGEN', 'imagem': ['../../../assets/media/volante.jpg'] },
+
+    //   { 'nome': 'BARRA DE DIREÇÃO AXIAL COM ROSCA FINA FIAT MOBI NOVO UNO NOVO PALIO NOVO FIORINO E GRAND SIENA TODAS AS VERSÕES MECÂNICO E HIDRÁULICO', 'tipo': 'Direção', 'preco': 188.50, 'quantidade': 9, 'marca': 'AXIAL', 'imagem': ['../../../assets/media/barra.jpg'] },
+
+    //   {
+    //     'nome': `MACACO JACARÉ PORTÁTIL 2 TONELADAS COM MALETA`, 'tipo': 'Ferramentas', 'preco': 137.00, 'quantidade': 10, 'marca': 'JACARÉ', 'imagem': ['../../../assets/media/Macaco.jpg']
+    //   },
+
+
+    // ];
 
   }
 
   ngOnInit() {
+    // this.productsService.getProdutos().subscribe((dados: any) => {
+    //   this.produtos = dados;
+    // });
   }
+
   openModal() {
     this.showModal = true;
   }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from 'selenium-webdriver/http';
+import { Observable } from 'rxjs';
+import { Http } from '@angular/http';
+// import { Http, respo } from '@angular/http'
+// import { IProdutos } from '../model/pruducts'  
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  // public produto = Produtos;
+
+  constructor(private http: HttpClient) { }
+
+  // getProdutos(): Observable<Produtos>{
+
+  //   this.http.get<Produtos>(`http://api-pacientes.herokuapp.com/pacientes`)
+  //   .subscribe(
+  //     res=>{
+  //       this.produto = res;
+  //     },
+  //     err=>{
+  //       console.log(err);
+  //     }
+  //   );
+  // }
+
+  // getProdutos():Observable<Produtos[]>{
+  //   // const url = 'http://api-pacientes.herokuapp.com/pacientes';~
+      
+  //   return this.http.get<Produtos[]>('http://api-pacientes.herokuapp.com/pacientes');
+  // }
+
 }
+
+// class Produtos{
+//   nome: string;
+//   peso: number;
+//   altura: number;
+//   gordura: number;
+//   imc: number;
+// }
